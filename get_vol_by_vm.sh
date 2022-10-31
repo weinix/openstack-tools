63cb1713ce61734cc084692768078a59826e608b
Wei Wang <wwang@infinidat.com>
2022-10-31 12:30:24 -0500

    Update vol query script.

--- get_vol_by_vm.sh
+++ get_vol_by_vm.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/env sh
-#Purpose: Query iSCSI volume information of a VM
-#Usage: ./$0 <vm_name>
+#!/bin/bash
 # Need admin rights to see query all information
 vm=$1
 
 ids=$(grep $vm servers.csv|awk '{print $2}')
 for id in $ids; do
-    virsh_name=$(openstack server show  $id -f json | jq '."OS-EXT-SRV-ATTR:instance_name"' | tr -d '"')
-    vols=$(grep $id volume_mapping.csv)
-    hypervisor=$(echo "$vols" | head -n1 | awk '{print $1}')
+    server_show_output=$(openstack server show $id -f json)
+    virsh_name=$( echo "$server_show_output" | jq '."OS-EXT-SRV-ATTR:instance_name"' | tr -d '"')
+    hypervisor=$( echo "$server_show_output" | jq '."OS-EXT-SRV-ATTR:hypervisor_hostname"' | tr -d '"')
+    vols=$( echo "$server_show_output" | jq '."volumes_attached"' | tr -d '"')
+    #vols=$(grep $id volume_mapping.csv)
     iqn=$(ssh heat-admin@$hypervisor  cat /etc/iscsi/initiatorname.iscsi)
     echo "======================================================================="
-    echo "compute=$hypervisor"
-    echo "iqn=$iqn"
-    echo "VM name=$vm"
-    echo "VM id=$id"
-    echo "Libvirt name=$virsh_name"
+    echo " hypervisor node: $hypervisor"
+    echo "             iqn: $iqn"
+    echo "         VM name: $vm"
+    echo "           VM id: $id"
+    echo "    Libvirt name: $virsh_name"
     echo "Volumes Attached:"
-    echo "$vols" | awk '{print $NF}'
+    echo -e "$vols"  | sed "s/^/  - /g"
 done
