3cc965152b322145dcc099346afe77958f66d178
Wei Wang <wwang@infinidat.com>
2022-10-31 12:47:15 -0500

    Update query vol script.

--- get_vol_by_vm.sh
+++ get_vol_by_vm.sh
@@ -8,12 +8,13 @@ for id in $ids; do
     virsh_name=$( echo "$server_show_output" | jq '."OS-EXT-SRV-ATTR:instance_name"' | tr -d '"')
     hypervisor=$( echo "$server_show_output" | jq '."OS-EXT-SRV-ATTR:hypervisor_hostname"' | tr -d '"')
     vols=$( echo "$server_show_output" | jq '."volumes_attached"' | tr -d '"')
+    vm_name=$( echo "$server_show_output" | jq '."name"' | tr -d '"')
     #vols=$(grep $id volume_mapping.csv)
     iqn=$(ssh heat-admin@$hypervisor  cat /etc/iscsi/initiatorname.iscsi)
     echo "======================================================================="
+    echo "         VM name: $vm_name"
     echo " hypervisor node: $hypervisor"
     echo "             iqn: $iqn"
-    echo "         VM name: $vm"
     echo "           VM id: $id"
     echo "    Libvirt name: $virsh_name"
     echo "Volumes Attached:"
